53dc55f51e769bacabda03607d1040aa21622bd8
Washington Luiz <huoxito@gmail.com>
2013-10-17 15:47:10 -0300

    try to debug payment source on heroku

---
+++ app/models/spree/gateway/adyen_payment.rb
@@ -0,0 +1,40 @@
+module Spree
+  class Gateway::AdyenPayment < Gateway
+    include AdyenCommon
+
+    def auto_capture?
+      false
+    end
+
+    def authorize(amount, source, gateway_options = {})
+      logger.debug source
+
+      provider.authorise_payment(
+        gateway_options[:order_id],
+
+        { :currency => Config.currency, :value => amount },
+
+        { :reference => gateway_options[:email],
+          :email => gateway_options[:email],
+          :ip => gateway_options[:ip],
+          :statement => "Order # #{gateway_options[:order_id]}" },
+
+        { :holder_name => "#{source.first_name} #{source.last_name}",
+          :number => source.number,
+          :cvc => source.verification_value,
+          :expiry_month => source.month,
+          :expiry_year => source.year }
+      )
+    end
+
+    def capture(amount, response_code, gateway_options = {})
+      value = { :currency => Config.currency, :value => amount }
+      response = provider.capture_payment(response_code, value)
+
+      # spree/payment/processing calls this method
+      def response.authorization; end
+
+      response
+    end
+  end
+end
